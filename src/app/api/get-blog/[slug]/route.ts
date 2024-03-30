877b84f3b8ce07abf2a04802bd83ae97cd102f6e
Princo1-bit <princolish@gmail.com>
2024-03-30 22:12:18 +0100

    removed nextrequest

--- src/app/api/get-blog/[slug]/route.ts
+++ src/app/api/get-blog/[slug]/route.ts
@@ -1,15 +1,12 @@
 import prisma from "@/lib/db/prisma";
 import { unstable_noStore as noStore } from "next/cache";
-import type NextRequest from "next/server.js";
-
-import type { NextApiRequest, NextApiResponse } from "next";
 
 type Params = {
   slug: string;
 };
 
 export const GET = async (
-  request: NextApiRequest,
+  
   context: { params: Params }
 ) => {
   noStore();
--- src/app/api/top-blogs/[take]/route.ts
+++ src/app/api/top-blogs/[take]/route.ts
@@ -1,13 +1,12 @@
 import prisma from "@/lib/db/prisma";
 import { unstable_noStore as noStore } from "next/cache";
-import { NextApiRequest } from "next/types";
 
 interface Prop {
   take: string
 }
 
 
-export async function GET( request: NextApiRequest, context: { params: Prop } ) {
+export async function GET( context: { params: Prop } ) {
   noStore()
 
   const { take } = context.params
